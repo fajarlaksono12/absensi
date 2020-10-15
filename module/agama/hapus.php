f09fc6c791726355dbbdd615f791fc71f60bf29e
fajarlaksono12 <fajarlaksono0212@gmail.com>
2020-10-15 23:25:04 +0700

    first commit

---
+++ module/agama/hapus.php
@@ -0,0 +1,22 @@
+<?php
+ include "koneksi.php";
+ $id=$_POST['id_agama'];
+ $query=mysqli_query($koneksi,"Delete FROM tb_agama WHERE id_agama='$id'");
+ 
+ 
+ if($query) // jika insert data berhasil
+ {
+  // fungsi untuk membuat format json
+  header('Content-Type: application/json');
+  // untuk load data yang sudah ada dari tabel
+  $content = file_get_contents('http://localhost:8080/absensi/module/hapus.php', true);
+  $data = array('status'=>'success', 'data'=> $agama);
+  echo json_encode($data);
+ }
+ else // jika insert data gagal
+ {
+  $data = array('status'=>'failed', 'data'=> null);
+  echo json_encode($data);
+ }
+ 
+?>
